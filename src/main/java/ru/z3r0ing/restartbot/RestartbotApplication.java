789f2a3326cd1541aa33f17c7202a8e4b710ee72
Даниил Лытин <lytin.daniil@gmail.com>
2022-07-10 19:55:01 +0800

    #3 activated scheduling

--- src/main/java/ru/z3r0ing/restartbot/RestartbotApplication.java
+++ src/main/java/ru/z3r0ing/restartbot/RestartbotApplication.java
@@ -2,8 +2,10 @@ package ru.z3r0ing.restartbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RestartbotApplication {
 
 	public static void main(String[] args) {
