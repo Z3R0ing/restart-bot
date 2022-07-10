cc5f24af4f55a14a0728ab9a27ddd4f1b9c26722
Даниил Лытин <lytin.daniil@gmail.com>
2022-07-10 17:08:04 +0800

    added try-catch to run application

--- src/main/java/ru/z3r0ing/restartbot/RestartbotApplication.java
+++ src/main/java/ru/z3r0ing/restartbot/RestartbotApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestartbotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestartbotApplication.class, args);
+		try {
+			SpringApplication.run(RestartbotApplication.class, args);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
